70fa169bd163d6a8444d600e26bf598ea44fba2b
Bimbika-Ganapathy <bimbikag@gmail.com>
2025-04-09 23:57:44 +0530

    exportes components

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-const { createServer } = require('node:http');
-const fs=require("fs");
-
-fs.read
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1> hello baba </h1>');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// import {a,b,c} from "./mymodule.js"
+// console.log(a, b, c);
+// import obj from "./mymodule.js";
+// console.log(obj);
 
 
-//to install express , npm install express
-// installs all the pkages to node modules 
-// if deleted then can restore using npm i
+const a = require("./mymodule2.js")
+console.log(a)
+
---
+++ mymodule2.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//     a:1,
+//     b: 9
+// }
+
+
+let c= 56;
+module.exports = c;
